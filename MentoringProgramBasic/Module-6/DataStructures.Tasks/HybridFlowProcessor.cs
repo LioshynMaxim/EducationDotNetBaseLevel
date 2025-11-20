7e905bb3bdb63eafca8ad5765f4f7d8f1d1d124c
Maksim_Lioshyn <maksimlioshyn@gmail.com>
2025-11-20 17:26:21 +0400

    Module 6 Final task 2

--- MentoringProgramBasic/Module-6/DataStructures.Tasks/HybridFlowProcessor.cs
+++ MentoringProgramBasic/Module-6/DataStructures.Tasks/HybridFlowProcessor.cs
@@ -1,28 +1,38 @@
 ﻿using DataStructures.Tasks.DoNotChange;
 using System;
+using System.Collections.Specialized;
 
 namespace DataStructures.Tasks
 {
     public class HybridFlowProcessor<T> : IHybridFlowProcessor<T>
     {
+        private DoublyLinkedList<T> _list = new DoublyLinkedList<T>();
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            if (_list.Length == 0)
+            {
+                throw new InvalidOperationException("The collection is empty.");
+            }
+            return _list.RemoveAt(0);
         }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            _list.AddAt(_list.Length, item);
         }
 
         public T Pop()
         {
-            throw new NotImplementedException();
+            if (_list.Length == 0)
+            {
+                throw new InvalidOperationException("The collection is empty.");
+            }
+            return _list.RemoveAt(_list.Length - 1);
         }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            _list.AddAt(_list.Length, item);
         }
     }
 }
